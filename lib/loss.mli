fbc4ed9b9ed338ca8ade076ceb72ce1fbd87e4ae
aeroplanepaper <2712641773@qq.com>
2024-11-11 17:03:20 -0500

    update nn

--- lib/loss.mli
+++
@@ -1,8 +0,0 @@
-
-type tensor = Tensor.t
-
-(** Mean squared error loss. *)
-val mse_loss : prediction:tensor -> target:tensor -> tensor
-
-(** Cross-entropy loss. *)
-val cross_entropy_loss : prediction:tensor -> target:tensor -> tensor
--- lib/nn.mli
+++ lib/nn.mli
@@ -1,55 +1,23 @@
+
+
 type tensor = Tensor.t
 
 (** The signature for a neural network module. *)
 module type Model = sig
-  type t
+  type t = {
+	forward: t -> tensor list -> tensor;
+	parameters: tensor list;
+  }
 
-  (** Forward pass through the module. *)
-  val init: t -> unit
+  val forward : t -> tensor list -> tensor
 
-  val forward : t -> tensor -> tensor
+  val linear : in_features:int -> out_features:int -> t
 
-  (** Get the parameters of the module. *)
-  val parameters : t -> tensor list
+  val squential : in_models:t list -> t
+
+  val relu : unit -> t
+
+  val mse_loss : unit -> t
+
 end
 
-(** Linear (fully connected) layer. *)
-module Linear : sig
-  type t
-
-  (** Create a linear layer with the given input and output sizes. *)
-  val create : in_features:int -> out_features:int -> t
-
-  (** Forward pass through the linear layer. *)
-  val forward : t -> tensor -> tensor
-
-  (** Get the parameters of the linear layer. *)
-  val parameters : t -> tensor list
-end
-
-module ReLU : sig
-  type t
-
-  (** Create a ReLU layer. *)
-  val create : unit -> t
-
-  (** Forward pass through the ReLU layer. *)
-  val forward : t -> tensor -> tensor
-
-  (** Get the parameters of the ReLU layer (empty list). *)
-  val parameters : t -> tensor list
-end
-
-(** Sequential container module. *)
-module Sequential : sig
-  type t
-
-  (** Create a sequential module from a list of modules. *)
-  val create : (module NNModule with type t = 'a) list -> t
-
-  (** Forward pass through the sequential module. *)
-  val forward : t -> tensor -> tensor
-
-  (** Get the parameters of all modules in the sequence. *)
-  val parameters : t -> tensor list
-end
